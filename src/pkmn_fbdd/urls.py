9a97c01a158af6225e92b12a5078927b8dcd059b
LuisCarri <luiscarrillo@ciencias.unam.mx>
2025-11-12 19:53:53 -0600

    Devuelve a todas las personas en postman

--- src/pkmn_fbdd/urls.py
+++ src/pkmn_fbdd/urls.py
@@ -6,4 +6,6 @@ app_name = 'pkmn_fbdd'
 urlpatterns = [
     path('', views.index, name='index'),
     path('personas/', views.lista_personas, name='lista_personas'),
+    path('api/personas/', views.api_personas, name='api_personas'),
+    path('api/personas/create/', views.api_persona_create, name='api_persona_create'),
 ]
